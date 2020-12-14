1d82167f90180c51442e1e8ef7241ff21cb654bc
Truong Duy <s3818381@rmit.edu.vn>
2020-12-14 12:27:27 +0700

    testscript

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 import './Component/custom.css'
 import Homepage from './Component/Homepage'
 
 function App() {
+  // this.useScript("./assets/vendor/bootstrap/css/bootstrap.min.css");
   return (
     <div className="App">
         <Homepage/>
       {/* Browsing in ReactJS */}
-      {/* Cái style thì em tham khảo this.useScript trong React */}
     </div>
   );
+}
+
+function useScript(src) {
+  const [ status, setStatus ] = useState(src ? "loading" : "idle");
 }
 
 export default App;
